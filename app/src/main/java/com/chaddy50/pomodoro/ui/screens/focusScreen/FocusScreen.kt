967e71f8f77eef263860eb1385befc764bf5f00e
chaddy50 <nchaddy50@protonmail.com>
2026-04-08 21:54:56 -0500

    Allow rotating to landscape even when device does not have auto-rotate enabled

--- app/src/main/java/com/chaddy50/pomodoro/ui/screens/focusScreen/FocusScreen.kt
+++ app/src/main/java/com/chaddy50/pomodoro/ui/screens/focusScreen/FocusScreen.kt
@@ -1,8 +1,12 @@
 package com.chaddy50.pomodoro.ui.screens.focusScreen
 
+import android.content.pm.ActivityInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.activity.ComponentActivity
 import com.chaddy50.pomodoro.ui.screens.focusScreen.mediaControls.MediaControlsViewModel
 import com.chaddy50.pomodoro.ui.screens.focusScreen.timer.TimerViewModel
 
@@ -12,6 +16,13 @@ fun FocusScreen(
     musicViewModel: MediaControlsViewModel,
     onStopFocus: () -> Unit,
 ) {
+    val activity = LocalContext.current as ComponentActivity
+    DisposableEffect(Unit) {
+        val original = activity.requestedOrientation
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+        onDispose { activity.requestedOrientation = original }
+    }
+
     val uiState by viewModel.uiState.collectAsState()
     val musicUiState by musicViewModel.uiState.collectAsState()
 
